1eb3453e67743ba2dca595eab65ba79bbfaebfd9
Sergey Obidin <player181285@gmail.com>
2021-10-29 10:11:58 +0400

    all program

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int balansNow = 2_000_000_000;
+        int transfer = 500_000_000;
+        int newBalans = balansNow + transfer;
+        System.out.println(newBalans);
     }
 }
